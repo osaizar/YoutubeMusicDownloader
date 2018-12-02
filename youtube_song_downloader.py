70b44ed7bd3c3b6e2ce111ea766896d0d65d803e
osaizar <osaizar@protonmail.com>
2018-12-02 18:22:55 +0100

    hobetuta

--- youtube_song_downloader.py
+++ youtube_song_downloader.py
@@ -7,16 +7,30 @@
 import pafy
 import os
 import urllib
-import urllib2
 import optparse
+import requests
 from bs4 import BeautifulSoup
 from pydub import AudioSegment
 
 FILE = "music_file.txt"
 OUTPUT_DIR = "music/"
+TMP_DIR = "/tmp/"
 RAR = False
 
+
 songs = []
+
+class Song:
+    def __init__(self, title, author=None):
+        self.title = title
+        self.author = author
+
+    def toStr(self):
+        rt = self.title
+        if self.author != None:
+            rt += " - "+self.author
+
+        return rt
 
 def parse_file():
     file = open(FILE, "r")
@@ -28,107 +42,86 @@ def parse_file():
                 author = line.split("::")[0]
             elif "\n" != line and "" != line:
                 if author != "?":
-                    songs.append(author+" - "+line)
+                    songs.append(Song(line, author))
                 else:
-                    songs.append(line)
+                    songs.append(Song(line))
 
 def make_dirs():
     for song in songs:
-        song_data = song.split(" - ")
-        if len(song_data) > 1 and not os.path.exists(OUTPUT_DIR+song_data[0]):
-            os.makedirs(OUTPUT_DIR+song_data[0])
+        if song.author != None:
+            if not os.path.exists(OUTPUT_DIR+song.author):
+                os.makedirs(OUTPUT_DIR+song.author)
 
 def get_link(song):
-    query = urllib.quote(song)
+    query = urllib.quote(song.toStr())
     url = "https://www.youtube.com/results?search_query=" + query
-    response = urllib2.urlopen(url)
+    response = urllib.urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html,"lxml")
     video = soup.findAll(attrs={'class':'yt-uix-tile-link'})[0]
     return 'https://www.youtube.com' + video['href']
 
 def get_audio(url):
-    print "checking URL..."
     video = pafy.new(url)
-    print "Got "+video.title
     return video.getbestaudio()
 
 def download_mp3(audio, song):
-    song_data = song.split(" - ")
-    raw_audio_title = audio.title+"."+audio.extension
-    if len(song_data) > 1:
-        mp3_audio_title = song_data[0]+"/"+song_data[1]+".mp3"
-    else:
-        mp3_audio_title = song_data[0]+".mp3"
+    lucky_header = {'Range': 'bytes=0-'}
+    fast_resp = requests.get(audio.url, headers=lucky_header)
+    with open(TMP_DIR+song.title+'.webm', 'wb') as fout:
+        fout.write(fast_resp.content)
 
-    print "Starting download..."
-    audio.download()
-    print "\nConverting to mp3..."
-    AudioSegment.from_file(raw_audio_title).export(OUTPUT_DIR+mp3_audio_title, format="mp3")
-    os.remove(raw_audio_title)
-    print "Done! \n"
+def convert_2_mp3(song):
+    if song.author == None:
+        path = song.title+".mp3"
+    else:
+        path = song.author+"/"+song.title+".mp3"
+
+    AudioSegment.from_file(TMP_DIR+song.title+'.webm').export(OUTPUT_DIR+path, format="mp3")
+    os.remove(TMP_DIR+song.title+'.webm')
 
 def zip_dir():
     print "ziping directory..."
     os.system("zip "+OUTPUT_DIR[:-1]+".zip "+OUTPUT_DIR[:-1]+" -rm")
     print "complete!  \n"
-    
-def shell():
-    exit = False
-    while not exit:
-        cmd = raw_input("shell>")
-        
-        if cmd == "exit":
-            exit = True
-            print("Bye!")
-        elif cmd[0:6] == "search":
-            arg = cmd.split(" ")
-            print("You typed search "+arg[1])
-        else:
-            print("'"+cmd+"' not found.")
 
 def parse_args():
-
     parser = optparse.OptionParser('Usage [-f <Music file>][-z][-s]')
 
     parser.add_option('-f', dest='file', type='string', \
                         help='music file')
     parser.add_option('-z', dest='rar', action="store_true", \
                         help='Makes a zip file for the output')
-    parser.add_option('-s', dest='shell', action="store_true", \
-                        help='Displays a shell')
 
     (options, args) = parser.parse_args()
 
     file = options.file
     rar = options.rar
-    shell = options.shell
-    
+
     if file == None:
         file = FILE
     if rar == None:
         rar = RAR
 
-    return rar, file, shell
+    return rar, file
 
 
 def main():
-    (RAR, FILE, SHELL) = parse_args()
-    if SHELL:
-        shell()
-    else:
-        parse_file()
-        make_dirs()
-        song_n = len(songs)
+    RAR, FILE = parse_args()
 
-        for i, song in enumerate(songs):
-            print str(i)+"/"+str(song_n)+" ["+str((i*100)/song_n)+"%] "+song
-            url = get_link(song)
-            audio = get_audio(url)
-            download_mp3(audio, song)
+    parse_file()
+    make_dirs()
+    song_n = len(songs)
 
-        if RAR:
-            zip_dir()
+    for i, song in enumerate(songs):
+        print str(i)+"/"+str(song_n)+" ["+str((i*100)/song_n)+"%] "+song.toStr()
+        url = get_link(song)
+        audio = get_audio(url)
+        download_mp3(audio, song)
+        convert_2_mp3(song)
+
+    if RAR:
+        zip_dir()
 
 if __name__ == '__main__':
     main()
